e03a70c11d3cd60274c9adf2c25338f319bbd482
iamnitesh <nitesh.swarnakar1@gmail.com>
2022-05-18 16:55:36 +0545

    second

--- src/pages/MyButton.js
+++ src/pages/MyButton.js
@@ -3,13 +3,7 @@ import React, { useRef } from "react";
 import { Stack, Alert } from "@material-ui/core";
 import "./button.css";
 function MyButton() {
-  let myref = useRef();
-
-  return (
-    <Stack spacing={2}>
-      <Alert severity="error">This is error</Alert>
-    </Stack>
-  );
+  return <></>;
 }
 
 export default MyButton;
